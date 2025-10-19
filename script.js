e7de2101bc0d484a7cccb3d040e1c6734d0700b2
LucasSQMoura <lucas.moura0404@gmail.com>
2025-10-19 16:51:37 -0300

    Excluir e alterar funcionarios

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Funcionario {
 const form = document.getElementById("formFuncionario");
 const tabela = document.getElementById("tabelaFuncionarios");
 let funcionarios = [];
+let editIndex = -1;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,21 +24,47 @@ form.addEventListener("submit", (e) => {
     document.getElementById("cargo").value,
     document.getElementById("salario").value
   );
-  funcionarios.push(f);
+
+  if (editIndex === -1) funcionarios.push(f);
+  else funcionarios[editIndex] = f;
+
   form.reset();
+  editIndex = -1;
   renderTabela();
 });
 
+// Alteração do renderTabela
 const renderTabela = () => {
   tabela.innerHTML = "";
-  funcionarios.forEach(f => {
+  funcionarios.forEach((f, i) => {
     const tr = document.createElement("tr");
     tr.innerHTML = `
       <td>${f.nome}</td>
       <td>${f.idade}</td>
       <td>${f.cargo}</td>
       <td>R$ ${f.salario.toFixed(2)}</td>
+      <td>
+        <button class="editar">Editar</button>
+        <button class="excluir">Excluir</button>
+      </td>
     `;
+    tr.querySelector(".editar").addEventListener("click", () => editarFuncionario(i));
+    tr.querySelector(".excluir").addEventListener("click", () => excluirFuncionario(i));
     tabela.appendChild(tr);
   });
 };
+
+const editarFuncionario = (i) => {
+  const f = funcionarios[i];
+  document.getElementById("nome").value = f.nome;
+  document.getElementById("idade").value = f.idade;
+  document.getElementById("cargo").value = f.cargo;
+  document.getElementById("salario").value = f.salario;
+  editIndex = i;
+};
+
+const excluirFuncionario = (i) => {
+  funcionarios.splice(i, 1);
+  renderTabela();
+};
+
